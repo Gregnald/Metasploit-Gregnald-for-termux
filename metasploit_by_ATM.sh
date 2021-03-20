b8f546c07d7c85abb8eb7d89095f22d48efbb263
avistnm <79373546+avistnm@users.noreply.github.com>
2021-03-20 11:45:16 +0530

    Update metasploit_by_ATM.sh

--- metasploit_by_ATM.sh
+++ metasploit_by_ATM.sh
@@ -5,37 +5,63 @@ echo -e "\e[34mINSTALLING....\e[0m"
 echo -e "\e[34mPACKAGES BEING INSTALLED WAIT....\e[0m"
 pkg update -y 
 pkg upgrade -y
-apt remove -y ruby
 echo -e "\e[34mPLEASE WAIT THIS CAN TAKE UP TO 5 MINUTES....\e[0m"
 apt install -y libiconv zlib autoconf bison clang coreutils curl findutils git apr apr-util libffi libgmp libpcap postgresql readline libsqlite openssl libtool libxml2 libxslt ncurses pkg-config wget make libgrpc termux-tools ncurses-utils ncurses unzip zip tar termux-elf-cleaner
 echo -e "\e[34mPACKAGES INSTALLED SUCCESSFULLY....[\e[92m✓\e[34m]\e[0m"
 echo -e "\033[92m"
 echo -e "\e[34mINSTALLING METASPLOIT....\e[0m"
-cd $HOME
-ln -sf $PREFIX/include/libxml2/libxml $PREFIX/include/
-ver='6.0.27'
-cd
-apt-mark unhold ruby
-curl -LO https://github.com/rapid7/metasploit-framework/archive/$ver.tar.gz
-cd
-tar -xf $ver.tar.gz
-cd
+#!/data/data/com.termux/files/usr/bin/bash
 
-apt install -y ./ruby.deb
-apt install -y ./rubyarm.deb
-apt-mark hold ruby
-cd metasploit-framework
-bundle config build.nokogiri --use-system-libraries 
-cd
+# Remove  Old Folder if exist 
+find $HOME -name "metasploit-*" -type d -exec rm -rf {} \;
+
+
+cwd=$(pwd)
+msfvar=6.0.33
+msfpath='/data/data/com.termux/files/home'
+
+apt update && apt upgrade
+# Temporary 
+apt remove ruby -y
+apt install -y libiconv zlib autoconf bison clang coreutils curl findutils git apr apr-util libffi libgmp libpcap postgresql readline libsqlite openssl libtool libxml2 libxslt ncurses pkg-config wget make ruby2 libgrpc termux-tools ncurses-utils ncurses unzip zip tar termux-elf-cleaner
+# Many phones are claiming libxml2 not found error
+ln -sf $PREFIX/include/libxml2/libxml $PREFIX/include/
+
+cd $msfpath
+curl -LO https://github.com/rapid7/metasploit-framework/archive/$msfvar.tar.gz
+
+tar -xf $msfpath/$msfvar.tar.gz
+mv $msfpath/metasploit-framework-$msfvar $msfpath/metasploit-framework
+cd $msfpath/metasploit-framework
+
+# Update rubygems-update
+if [ "$(gem list -i rubygems-update 2>/dev/null)" = "false" ]; then
+	gem install --no-document --verbose rubygems-update
+fi
+
+# Update rubygems
+update_rubygems
+
+# Install bundler
+gem install --no-document --verbose bundler:1.17.3
+
+# Installing all gems 
+bundle config build.nokogiri --use-system-libraries
+bundle install -j3
+echo "Gems installed"
+
+# Some fixes
+sed -i "s@/etc/resolv.conf@$PREFIX/etc/resolv.conf@g" $msfpath/metasploit-framework/lib/net/dns/resolver.rb
+find "$msfpath"/metasploit-framework -type f -executable -print0 | xargs -0 -r termux-fix-shebang
+find "$PREFIX"/lib/ruby/gems -type f -iname \*.so -print0 | xargs -0 -r termux-elf-cleaner
+
 echo "Creating database"
-mv metasploit-framework-6.0.27 metasploit-framework
-mkdir -p metasploit-framework/config && cd metasploit-framework/config
+
+mkdir -p $msfpath/metasploit-framework/config && cd $msfpath/metasploit-framework/config
 curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/database.yml
 
-cd
-
 mkdir -p $PREFIX/var/lib/postgresql
-pg_ctl -D "$PREFIX"/var/lib/postgresql stop || true
+pg_ctl -D "$PREFIX"/var/lib/postgresql stop > /dev/null 2>&1 || true
 
 if ! pg_ctl -D "$PREFIX"/var/lib/postgresql start --silent; then
     initdb "$PREFIX"/var/lib/postgresql
@@ -48,13 +74,14 @@ if [ -z "$(psql -l | grep msf_database)" ]; then
     createdb msf_database
 fi
 
+rm $msfpath/$msfvar.tar.gz
+
 cd
 
 chmod 777 msfconsole
 mv msfconsole /data/data/com.termux/files/usr/bin
 ln -sf $(which msfconsole) $PREFIX/bin/msfvenom
 
-rm $ver.tar.gz
 cd
 rm ruby.deb
 rm rubyarm.deb
@@ -63,10 +90,20 @@ rm -rf Metasploit-avistnm
 rm -f metasploit_by_ATM.sh
 rm -rf msfvenom
 
+echo -e "\e[1;46m Fixing in Progress..... \e[0m"
+cd
+rm -rf avistnm-Ruby-Fixer-Metasploit
+apt remove -y ruby
+apt install -y ./ruby.deb
+apt install -y ./rubyarm.deb
 cd metasploit-framework
+bundle config build.nokogiri --use-system-libraries 
 bundle install
 bundle update
-cd
+echo -e "\e[1;46m Fixing Over!! \e[0m"
+
+sleep 6.0
+
 clear
 echo -e "\e[1;42m                    Installed!! \e[0m"
 echo -e "\e[1;46m Now you can directly use msfvenom or msfconsole rather than ./msfvenom or ./ms
