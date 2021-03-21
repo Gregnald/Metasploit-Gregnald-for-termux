0f246b011f0f1ae6bafaa16ad34962a925d75073
Aviral Srivastava <79373546+avistnm@users.noreply.github.com>
2021-03-22 01:48:17 +0530

    Update metasploit_by_ATM.sh

--- metasploit_by_ATM.sh
+++ metasploit_by_ATM.sh
@@ -1,11 +1,19 @@
+#ruby 2.7.0 install
+cd
 apt install -y ./ruby.deb
+apt install -y ./rubyarm.deb
+
+#lolcat
 gem install lolcat
+
 lolcat timewarn.sh
 echo -e "\e[34m Depending On Your Network Speed\e[0m"
-Sleep 6.0
+sleep 6.0
 cd
 echo -e "\e[34mREMOVING OLD METASPLOIT FOLDER(if any)....\e[0m"
-rm -rf metasploit-framework
+# Remove  Old Folder if exist 
+find $HOME -name "metasploit-*" -type d -exec rm -rf {} \;
+
 echo -e "\e[34mINSTALLING....\e[0m"
 echo -e "\e[34mPACKAGES BEING INSTALLED WAIT....\e[0m"
 pkg update -y 
@@ -17,9 +25,6 @@ echo -e "\033[92m"
 echo -e "\e[34mINSTALLING METASPLOIT....\e[0m"
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Remove  Old Folder if exist 
-find $HOME -name "metasploit-*" -type d -exec rm -rf {} \;
-
 
 cwd=$(pwd)
 msfvar=6.0.33
@@ -27,13 +32,9 @@ msfpath='/data/data/com.termux/files/home'
 
 apt update && apt upgrade
 # Temporary 
-apt remove ruby -y
 apt install -y libiconv zlib autoconf bison clang coreutils curl findutils git apr apr-util libffi libgmp libpcap postgresql readline libsqlite openssl libtool libxml2 libxslt ncurses pkg-config wget make ruby2 libgrpc termux-tools ncurses-utils ncurses unzip zip tar termux-elf-cleaner
 # Many phones are claiming libxml2 not found error
 ln -sf $PREFIX/include/libxml2/libxml $PREFIX/include/
-
-#ruby 2.7.x
-apt install -y ./ruby.deb
 
 cd $msfpath
 curl -LO https://github.com/rapid7/metasploit-framework/archive/$msfvar.tar.gz
@@ -90,17 +91,6 @@ chmod 777 msfconsole
 mv msfconsole /data/data/com.termux/files/usr/bin
 ln -sf $(which msfconsole) $PREFIX/bin/msfvenom
 
-#Remove Ruby
-apt remove ruby -y
-
-#Install Ruby
-cd
-apt install -y ./ruby.deb
-apt install -y ./rubyarm.deb
-
-#lolcat
-gem install lolcat
-
 cd
 rm ruby.deb
 rm rubyarm.deb
@@ -111,8 +101,6 @@ rm -rf msfvenom
 rm -f database.yml
 
 echo -e "\e[1;34m Fixing in Progress..... \e[0m"
-cd
-apt remove -y ruby
 cd metasploit-framework
 bundle config build.nokogiri --use-system-libraries 
 bundle install
